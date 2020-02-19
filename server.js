151d332e5f3b7e7f01f6c2bca066c5f4fdfe5e83
Victor Lindquist <vicli268@student.liu.se>
2020-02-19 02:47:58 +0100

    SEO: fixed gzipping

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const express = require('express');
-const expressStaticGzip = require("express-static-gzip");
 const favicon = require('express-favicon');
+const compression = require('compression');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.use(compression());//gzip files
+
 // the __dirname is the current directory from where the script is running
 app.use(favicon(__dirname + '/build/favicon.ico'));
-
-app.use(
-  expressStaticGzip(path.join(__dirname, 'build'), {
-  enableBrotli: true, // only if you have brotli files too
-  }),
-); //added!
 
 app.use(express.static(__dirname));
 
